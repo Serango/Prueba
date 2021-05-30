e82018f1ee927b261cb7437cbdea07ae8c57fa7f
Acer <Acer@LAPTOP-32FOQR5E>
2021-05-30 15:27:39 +0200

    Esto es una prueba

---
+++ prueba1/src/cambio/Cambio.java
@@ -0,0 +1,14 @@
+package cambio;
+
+public class Cambio {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Pruebas Git. Clase cambio");
+		
+		
+		
+
+	}
+
+}
---
+++ prueba1/src/cambio/Principal.java
@@ -0,0 +1,21 @@
+package cambio;
+
+public class Principal {
+
+	public static void main(String [] args) {
+		
+		System.out.println("Hola mundo java-git");
+		
+		System.out.println("Hola mundo java-git");
+		
+		System.out.println("Hola mundo java-git");
+		
+		System.out.println("Pruebas Git. Se añade Cambio");
+		
+	}
+	
+	
+	
+	
+	
+}
